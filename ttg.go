87181135eb81e38ccd2ba385f2e690d3bc28a18d
Alex von Brandenfels <alex.brandenfels@gmail.com>
2017-03-09 13:43:07 -0800

    Gave board more flexibility, & added String method

--- ttg.go
+++ ttg.go
@@ -1,34 +1,61 @@
 package ttgBoard
 
-var PLAYER_1 = 1
-var PLAYER_2 = 2
+import (
+	"strconv"
+	"strings"
+)
 
-type Board struct {
-	BoardState [3][3]int
-	WhoseTurn int
+type gameBoard struct {
+	BoardState [][]int
+	WhoseTurn, NumPlayers, Cols, Rows, WinNumber int
+}
+
+// Returns a new instance of a gameBoard.
+func NewBoard(numRows, numCols, numPlayers, winNumber int) *gameBoard {
+	board := new(gameBoard)
+	board.WhoseTurn = 1
+	board.Rows = numRows
+	board.Cols = numCols
+	board.NumPlayers = numPlayers
+	board.WinNumber = winNumber
+	board.BoardState = make([][]int, numRows, numRows)
+	for i := range board.BoardState {
+		board.BoardState[i] = make([]int, numCols, numCols)
+	}
+	return board
 }
 
 // Returns true only if the given space is not already occupied.
-func (board Board) isValidTurn(xpos, ypos int) bool {
+func (board gameBoard) isValidTurn(xpos, ypos int) bool {
 	return board.BoardState[ypos][xpos] == 0
 }
 
-// Changes which player's turn it is.
-func (board Board) swapWhoseTurn() {
-	switch turn := board.WhoseTurn {
-	case PLAYER_1:
-		board.WhoseTurn = PLAYER_2
-	default:
-		board.WhoseTurn = PLAYER_1
-	}
+// Increments which player's turn it is.
+func (board gameBoard) changeWhoseTurn() {
+	player := board.WhoseTurn
+	player = player % board.NumPlayers
+	player += 1
+	board.WhoseTurn = player
 }
 
 // Attempts to make specified move. If invalid move, returns false.
-func (board Board) TakeTurn(xpos, ypos int) bool {
+func (board gameBoard) TakeTurn(xpos, ypos int) bool {
 	if ! board.isValidTurn(xpos, ypos) {
 		return false
 	}
 	board.BoardState[ypos][xpos] = board.WhoseTurn
-	board.swapWhoseTurn()
+	board.changeWhoseTurn()
 	return true
+}
+
+// Returns the game board represented as a string
+func (board gameBoard) String() string {
+	var results []string
+	for _, row := range board.BoardState {
+		for _, cellVal := range row {
+			results = append(results, strconv.Itoa(cellVal))
+		}
+		results = append(results, "\n")
+	}
+	return strings.Join(results, "")
 }
